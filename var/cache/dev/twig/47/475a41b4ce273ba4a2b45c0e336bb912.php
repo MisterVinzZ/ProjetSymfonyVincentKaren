7f4e8dbe12b2f7465613c9d17b418288d517a682
Aldehuelo <vincentime77@live.fr>
2024-04-12 15:37:24 +0200

    style

--- var/cache/dev/twig/47/475a41b4ce273ba4a2b45c0e336bb912.php
+++ var/cache/dev/twig/47/475a41b4ce273ba4a2b45c0e336bb912.php
@@ -37,17 +37,10 @@ class __TwigTemplate_7482410b0875c0e525331d194bb94157 extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "template", "produit/index.html.twig"));
 
-        // line 2
+        // line 1
         echo "
-    ";
-        // line 3
-        $this->loadTemplate("includes/navbar.html.twig", "produit/index.html.twig", 3)->display($context);
-        // line 4
-        echo "
-
-
 ";
-        // line 7
+        // line 2
         $this->displayBlock('body', $context, $blocks);
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
@@ -60,59 +53,91 @@ class __TwigTemplate_7482410b0875c0e525331d194bb94157 extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "body"));
 
-        // line 8
+        // line 3
         echo "    <link rel=\"stylesheet\" href=\"";
         echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("css/produits.css"), "html", null, true);
         echo "\">
     <link rel=\"stylesheet\" href=\"";
-        // line 9
+        // line 4
         echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("css/styles.css"), "html", null, true);
         echo "\">
 
     <h1>Liste des Figurines</h1>
 
     <div class=\"container\">
-        ";
-        // line 14
-        $context['_parent'] = $context;
-        $context['_seq'] = twig_ensure_traversable((isset($context["produits"]) || array_key_exists("produits", $context) ? $context["produits"] : (function () { throw new RuntimeError('Variable "produits" does not exist.', 14, $this->source); })()));
-        foreach ($context['_seq'] as $context["_key"] => $context["produit"]) {
-            // line 15
-            echo "        <div class=\"produit\">
-            <a href=\"";
-            // line 16
-            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("produit_show", ["id" => twig_get_attribute($this->env, $this->source, $context["produit"], "id", [], "any", false, false, false, 16)]), "html", null, true);
-            echo "\">
-                <img src=\"";
-            // line 17
-            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl(("images/" . twig_get_attribute($this->env, $this->source, $context["produit"], "image", [], "any", false, false, false, 17))), "html", null, true);
-            echo "\" alt=\"";
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["produit"], "nom", [], "any", false, false, false, 17), "html", null, true);
-            echo "\">
-            
-            <div class=\"produit-info\">
-                <div class=\"produit-nom\">";
-            // line 20
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["produit"], "nom", [], "any", false, false, false, 20), "html", null, true);
-            echo "</div>
-                <div class=\"produit-description\">";
-            // line 21
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["produit"], "description", [], "any", false, false, false, 21), "html", null, true);
-            echo "</div>
-                <div class=\"produit-prix\">";
-            // line 22
-            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["produit"], "prix", [], "any", false, false, false, 22), "html", null, true);
-            echo "€</div>
-            </div>
-            </a>
+        <div class=\"sidebar\">
+            <h2>Catégories</h2>
+            <select id=\"categorieSelect\" onchange=\"filterProducts()\">
+                <option value=\"tous\">Tous les produits</option>
+                <option value=\"figurines\">Figurines</option>
+                <option value=\"accessoires\">Accessoires</option>
+                <option value=\"deco\">Décorations</option>
+                <option value=\"vetement\">vetement</option>
+
+                <!-- Ajoutez d'autres options pour les différentes catégories -->
+            </select>
         </div>
-    ";
+
+        <div class=\"produits-list\" id=\"produitsList\">
+            ";
+        // line 23
+        $context['_parent'] = $context;
+        $context['_seq'] = twig_ensure_traversable((isset($context["produits"]) || array_key_exists("produits", $context) ? $context["produits"] : (function () { throw new RuntimeError('Variable "produits" does not exist.', 23, $this->source); })()));
+        foreach ($context['_seq'] as $context["_key"] => $context["produit"]) {
+            // line 24
+            echo "                <div class=\"produit ";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["produit"], "categorie", [], "any", false, false, false, 24), "html", null, true);
+            echo "\">
+                    <a href=\"";
+            // line 25
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\RoutingExtension']->getPath("produit_show", ["id" => twig_get_attribute($this->env, $this->source, $context["produit"], "id", [], "any", false, false, false, 25)]), "html", null, true);
+            echo "\">
+                        <img src=\"";
+            // line 26
+            echo twig_escape_filter($this->env, $this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl(("images/" . twig_get_attribute($this->env, $this->source, $context["produit"], "image", [], "any", false, false, false, 26))), "html", null, true);
+            echo "\" alt=\"";
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["produit"], "nom", [], "any", false, false, false, 26), "html", null, true);
+            echo "\">
+                        <div class=\"produit-info\">
+                            <div class=\"produit-nom\">";
+            // line 28
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["produit"], "nom", [], "any", false, false, false, 28), "html", null, true);
+            echo "</div>
+                            <div class=\"produit-description\">";
+            // line 29
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["produit"], "description", [], "any", false, false, false, 29), "html", null, true);
+            echo "</div>
+                            <div class=\"produit-prix\">";
+            // line 30
+            echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, $context["produit"], "prix", [], "any", false, false, false, 30), "html", null, true);
+            echo "€</div>
+                        </div>
+                    </a>
+                </div>
+            ";
         }
         $_parent = $context['_parent'];
         unset($context['_seq'], $context['_iterated'], $context['_key'], $context['produit'], $context['_parent'], $context['loop']);
         $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 27
-        echo "    </div>
+        // line 35
+        echo "        </div>
+    </div>
+
+    <script>
+        function filterProducts() {
+            var selectedCategory = document.getElementById('categorieSelect').value;
+            var produits = document.querySelectorAll('.produit');
+            var produitsList = document.getElementById('produitsList');
+            
+            produits.forEach(function(produit) {
+                if (selectedCategory === 'tous' || produit.classList.contains(selectedCategory)) {
+                    produit.style.display = 'block';
+                } else {
+                    produit.style.display = 'none';
+                }
+            });
+        }
+    </script>
 
 ";
         
@@ -131,27 +156,14 @@ class __TwigTemplate_7482410b0875c0e525331d194bb94157 extends Template
     /**
      * @codeCoverageIgnore
      */
-    public function isTraitable()
-    {
-        return false;
-    }
-
-    /**
-     * @codeCoverageIgnore
-     */
     public function getDebugInfo()
     {
-        return array (  115 => 27,  104 => 22,  100 => 21,  96 => 20,  88 => 17,  84 => 16,  81 => 15,  77 => 14,  69 => 9,  64 => 8,  51 => 7,  46 => 4,  44 => 3,  41 => 2,);
+        return array (  123 => 35,  112 => 30,  108 => 29,  104 => 28,  97 => 26,  93 => 25,  88 => 24,  84 => 23,  62 => 4,  57 => 3,  44 => 2,  41 => 1,);
     }
 
     public function getSourceContext()
     {
-        return new Source("{# templates/produit/index.html.twig #}
-
-    {% include 'includes/navbar.html.twig' %}
-
-
-
+        return new Source("
 {% block body %}
     <link rel=\"stylesheet\" href=\"{{ asset('css/produits.css') }}\">
     <link rel=\"stylesheet\" href=\"{{ asset('css/styles.css') }}\">
@@ -159,20 +171,50 @@ class __TwigTemplate_7482410b0875c0e525331d194bb94157 extends Template
     <h1>Liste des Figurines</h1>
 
     <div class=\"container\">
-        {% for produit in produits %}
-        <div class=\"produit\">
-            <a href=\"{{ path('produit_show', {'id': produit.id}) }}\">
-                <img src=\"{{ asset('images/' ~ produit.image) }}\" alt=\"{{ produit.nom }}\">
-            
-            <div class=\"produit-info\">
-                <div class=\"produit-nom\">{{ produit.nom }}</div>
-                <div class=\"produit-description\">{{ produit.description }}</div>
-                <div class=\"produit-prix\">{{ produit.prix }}€</div>
-            </div>
-            </a>
+        <div class=\"sidebar\">
+            <h2>Catégories</h2>
+            <select id=\"categorieSelect\" onchange=\"filterProducts()\">
+                <option value=\"tous\">Tous les produits</option>
+                <option value=\"figurines\">Figurines</option>
+                <option value=\"accessoires\">Accessoires</option>
+                <option value=\"deco\">Décorations</option>
+                <option value=\"vetement\">vetement</option>
+
+                <!-- Ajoutez d'autres options pour les différentes catégories -->
+            </select>
         </div>
-    {% endfor %}
+
+        <div class=\"produits-list\" id=\"produitsList\">
+            {% for produit in produits %}
+                <div class=\"produit {{ produit.categorie }}\">
+                    <a href=\"{{ path('produit_show', {'id': produit.id}) }}\">
+                        <img src=\"{{ asset('images/' ~ produit.image) }}\" alt=\"{{ produit.nom }}\">
+                        <div class=\"produit-info\">
+                            <div class=\"produit-nom\">{{ produit.nom }}</div>
+                            <div class=\"produit-description\">{{ produit.description }}</div>
+                            <div class=\"produit-prix\">{{ produit.prix }}€</div>
+                        </div>
+                    </a>
+                </div>
+            {% endfor %}
+        </div>
     </div>
+
+    <script>
+        function filterProducts() {
+            var selectedCategory = document.getElementById('categorieSelect').value;
+            var produits = document.querySelectorAll('.produit');
+            var produitsList = document.getElementById('produitsList');
+            
+            produits.forEach(function(produit) {
+                if (selectedCategory === 'tous' || produit.classList.contains(selectedCategory)) {
+                    produit.style.display = 'block';
+                } else {
+                    produit.style.display = 'none';
+                }
+            });
+        }
+    </script>
 
 {% endblock %}
 ", "produit/index.html.twig", "C:\\xampp\\htdocs\\ProjetEcommerce\\templates\\produit\\index.html.twig");
